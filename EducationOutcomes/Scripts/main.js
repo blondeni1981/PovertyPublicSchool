3c0d863395c2910faf83d3c9f70bdc47de737e33
mccrackenn <mccrackenn@gmail.com>
2021-06-08 12:28:11 -0700

    SchoolPerfomance Query almost working

--- EducationOutcomes/Scripts/main.js
+++ EducationOutcomes/Scripts/main.js
@@ -1,5 +1,7 @@
 ﻿var uri = 'api/Outcomes';
-mySchoolArray = [];
+let mySchoolArray = [];
+let ctx = document.getElementById('myChart').getContext('2d');
+let myChart;
 
 $(document).ready(function () {
     fetch(uri).then(function (response) {
@@ -33,4 +35,62 @@ GetSchools = () => {
         console.log(percentageData);
         $('#school-list').append(percentageData[0].School1 + " students have a poverty percentage of: " + percentageData[0].PovPercent);
     })
+}
+
+GetSchoolPerformance = () => {
+    var radioValue = $("input[name='performance']:checked").val();
+    console.log(radioValue);
+    fetch(uri + '/getSchoolPerformance?topLow=' + radioValue).then(function (response) {
+        if (response.ok) {
+            return response.json();
+        } else {
+            return Promise.reject(response);
+        }
+    }).then((performanceData) => {
+        console.log(performanceData);
+        DrawChart();
+    })
+}
+
+DrawChart = () => {
+    
+    
+    
+    ctx.clearRect(0, 0, ctx.width, ctx.height);
+    if (typeof myChart !== 'undefined') {
+        myChart.destroy();
+    }
+    
+     myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            responsive: false
+        }
+
+
+    });
 }
