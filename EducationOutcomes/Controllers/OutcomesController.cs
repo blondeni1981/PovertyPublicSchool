82c3cb064e761e5e4ad23a7b727a903d4aa474c6
Blondeni1981 <nicolemari922@gmail.com>
2021-06-14 11:10:55 -0700

    Push new queries, almost done......

--- EducationOutcomes/Controllers/OutcomesController.cs
+++ EducationOutcomes/Controllers/OutcomesController.cs
@@ -39,18 +39,18 @@ namespace EducationOutcomes.Controllers
         public IEnumerable<object> GetSchoolPerformance(string topLow)
         {
             var result = from school in povertyEntities.TestResults
-                         select new {school.School.School1, school.PercentMetStandard };
+                         select new { school.School.School1, school.PercentMetStandard };
 
-            
+
             var newResult = from a in result
                             group a by a.School1 into aGroup
                             select new
                             {
-                                School=aGroup.Key,
+                                School = aGroup.Key,
                                 AverageGrade = aGroup.Average(b => b.PercentMetStandard)
-                                
+
                             };
-            if(topLow == "top")
+            if (topLow == "top")
             {
                 return newResult.OrderByDescending(a => a.AverageGrade).Take(10).ToList();
             }
@@ -58,21 +58,87 @@ namespace EducationOutcomes.Controllers
             {
                 return newResult.OrderBy(a => a.AverageGrade).Take(10).ToList();
             }
-            
+
         }
-//        select top 10 AVG(PercentMetStandard) as SchoolAverage, S.School
-//from TestResults as TR
-//    inner join School as S
-//    on S.SchoolId=TR.SchoolId
-//    inner join District as D
-//    on D.DistrictId=TR.DistrictId
-//    inner join Subject
-//    on Subject.SubjectId=TR.SubjectId
-//where Subject.SubjectId= 1
-//Group by S.School
-//order by SchoolAverage desc
+
+
+
+
+
+        //------------------------------------socioEcoStatus---------------------------------------
+        PublicSchoolPovertyEntities db = new PublicSchoolPovertyEntities();
+
+        [System.Web.Http.Route("socio")]
+        [System.Web.Http.HttpGet]
+        public IEnumerable<object> GetSocioList()
+        {
+            var SocioQuery = from s in db.SocioeconomicStatus
+                             join d in db.TestResults on s.SocioeconomicStatusId equals d.SocioeconomicStatusId
+                             join gr in db.GraduationRates on s.SocioeconomicStatusId equals gr.GraduationId
+                             select new
+                             {
+                                 s.SocioeconomicStatus
+
+                             };
+            return SocioQuery.Distinct().ToList();
+        }
+
+        //----------------------------------------School Dropdown---------------------------------------------------------------
+
+
+
+        [System.Web.Http.Route("school")]
+        [System.Web.Http.HttpGet]
+        public IEnumerable<object> GetSchoolList()
+        {
+            var SocioQuery = from s in db.Schools
+                             join d in db.TestResults on s.SchoolId equals d.SchoolId
+                             join gr in db.GraduationRates on s.SchoolId equals gr.SchoolId
+                             join socio in db.SocioeconomicStatus on d.SocioeconomicStatusId equals socio.SocioeconomicStatusId
+                             where s.School1 == s.School1 && socio.SocioeconomicStatus == "Homeless"
+                                     || socio.SocioeconomicStatus == "Non-Homeless"
+                             select new
+                             {
+                                 s.School1
+
+                             };
+            return SocioQuery.Distinct().ToList();
+        }
+
+        [System.Web.Http.Route("provSchool")]
+        [System.Web.Http.HttpGet]
+        public IHttpActionResult GetPovSchool(string id, string id2)
+        {
+            try
+            {
+                var PovSchoolQuery = from gr in db.GraduationRates
+                                     join d in db.Districts on gr.DistrictId equals d.DistrictId
+                                     join pov in db.Poverties on gr.DistrictId equals pov.DistrictId
+                                     join sch in db.Schools on gr.SchoolId equals sch.SchoolId
+                                     join socio in db.SocioeconomicStatus on gr.SocioeconomicStatusId equals socio.SocioeconomicStatusId
+                                     join tr in db.TestResults on gr.DistrictId equals tr.DistrictId
+                                     where pov.SchoolId == sch.SchoolId && sch.SchoolId == tr.SchoolId
+                                     && socio.SocioeconomicStatusId == tr.SocioeconomicStatusId
+                                     where sch.School1 == id && socio.SocioeconomicStatus == id2
+                                     group new { gr.GraduationRate1, tr.PercentMetStandard }
+                                     by new { d.DistrictName, sch.School1, socio.SocioeconomicStatus, pov.PovPercent }
+                                     into SocioGroup
+                                     orderby SocioGroup.Key.PovPercent
+                                     select new
+                                     {
+                                         DistrictName = SocioGroup.Key.DistrictName,
+                                         School1 = SocioGroup.Key.School1,
+                                         SocioeconomicStatus = SocioGroup.Key.SocioeconomicStatus,
+                                         PovPercent = SocioGroup.Key.PovPercent,
+                                         GraduationRate1 = SocioGroup.Average(p => p.GraduationRate1),
+                                         TestResults = SocioGroup.Average(p => p.PercentMetStandard)
+                                     };
+                return Ok(PovSchoolQuery);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+        }
     }
-
-
-
 }
--- EducationOutcomes/Scripts/main.js
+++ EducationOutcomes/Scripts/main.js
@@ -144,3 +144,94 @@ DrawPieChart = (school, percentage) => {
         }
     });
 }
+
+////Function to populate Socio drop down
+$(function () {
+    $.ajax({
+        type: "GET",
+        url: 'api/Outcomes/socio',
+        data: '{}',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (r) {
+            $.each(r, function (key, socio) {
+                $('#socio1').append($('<option></option>').val(socio.$id).html(socio.SocioeconomicStatus));
+            });
+        }
+    });
+});
+
+////Function to populate school drop down
+$(function () {
+    $.ajax({
+        type: "GET",
+        url: 'api/Outcomes/school',
+        data: '{}',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (r) {
+            console.log(r)
+            $.each(r, function (key, school) {
+                $('#school2').append($('<option></option>').val(school.$id).html(school.School1));
+            });
+        }
+    });
+});
+
+function GetPovList() {
+
+    var select = document.querySelector("#school2");
+    var select2 = document.querySelector("#socio1");
+    var id = select.options[select.selectedIndex].text;
+    var id2 = select2.options[select2.selectedIndex].text;
+    console.log(id);
+
+    $.ajax({
+        url: 'api/Outcomes/provSchool?id=' + id + '&id2=' + id2,
+        data: "",
+        type: "GET",
+        dataType: "json",
+        success: function (povCount) {
+            PovListSuccess(povCount);
+            console.log(povCount);
+        },
+        error: function (request, message, error) {
+            handleException(request, message, error);
+        }
+    });
+}
+
+function PovListSuccess(pov) {
+    // Iterate over the collection of data
+    $.each(pov, function (index, pov) {
+        // Add a row to the Order Detail table
+        PovAddRow(pov);
+        console.log(pov);
+    });
+}
+
+function PovAddRow(pov) {
+    // Check if <tbody> tag exists, add one if not
+    if ($("#PovTable tbody").length == 0) {
+        $("#PovTable").append("<tbody></tbody>");
+        console.log(pov);
+    }
+    // Append row to <table>
+    $("#PovTable tbody").append(
+        PovBuildTableRow(pov));
+}
+
+function PovBuildTableRow(pov) {
+    var ret =
+        "<tr>" +
+        "<td>" + pov.DistrictName + "</td>" +
+        "<td>" + pov.School1 + "</td>" +
+        "<td>" + pov.SocioeconomicStatus + "</td>" +
+        "<td>" + pov.PovPercent + "</td>" +
+        "<td>" + pov.GraduationRate1 + "</td>" +
+        "<td>" + pov.TestResults + "</td>" +
+        //"<td>" + pov. + "</td>" +
+        "</td>"
+    "</tr>";
+    return ret;
+}
